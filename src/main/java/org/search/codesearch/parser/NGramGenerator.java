418711d0b308627915b4f31be33b6253cd7eabf1
Ashkrit <ashkrit@gmail.com>
2020-07-26 21:12:49 +0800

    Test around 3 grams

--- src/main/java/org/search/codesearch/parser/NGramGenerator.java
+++ src/main/java/org/search/codesearch/parser/NGramGenerator.java
@@ -4,25 +4,42 @@ import java.util.function.Consumer;
 
 public class NGramGenerator {
 
-    public void generate(String text, int noOfGrams, Consumer<String> consumer) {
+    private final int noOfGrams;
+
+    public NGramGenerator(int noOfGrams) {
+        this.noOfGrams = noOfGrams;
+    }
+
+    public void generate(String text, Consumer<String> consumer) {
         if (noOfGrams == 0) {
-            for (String v : text.split(" ")) {
+            for (String v : extractParts(text)) {
                 consumer.accept(v);
             }
         }
 
+        if (noOfGrams < 3)
+            throw new IllegalArgumentException("Too small");
+
+
         if (noOfGrams == 3) {
-            String[] words = text.split(" ");
+            String[] words = extractParts(text);
             for (String word : words) {
-                if (word.length() >= noOfGrams) {
-                    for (int index = 0; index + 3 <= word.length(); index++) {
-                        consumer.accept(word.substring(index, index + 3));
+                if (nGramRequired(noOfGrams, word)) {
+                    for (int index = 0; index + noOfGrams <= word.length(); index++) {
+                        consumer.accept(word.substring(index, index + noOfGrams));
                     }
                 } else {
                     consumer.accept(word);
                 }
             }
         }
+    }
 
+    private String[] extractParts(String text) {
+        return text.split(" ");
+    }
+
+    private boolean nGramRequired(int noOfGrams, String word) {
+        return word.length() >= noOfGrams;
     }
 }
--- src/test/java/org/search/codesearch/parser/NGramParserTest.java
+++ src/test/java/org/search/codesearch/parser/NGramParserTest.java
@@ -6,26 +6,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class NGramParserTest {
 
 
     @Test
     public void generate_no_grams() {
-        NGramGenerator g = new NGramGenerator();
+        NGramGenerator g = new NGramGenerator(0);
         List<String> result = new ArrayList<>();
-        g.generate("this is simple text", 0, (String s) ->
-                result.add(s));
+
+        g.generate("this is simple text", (String s) -> result.add(s));
+
         assertArrayEquals(new String[]{"this", "is", "simple", "text"}, result.toArray(new String[]{}));
     }
 
 
     @Test
     public void generate_3_grams() {
-        NGramGenerator g = new NGramGenerator();
+        NGramGenerator g = new NGramGenerator(3);
+
         List<String> result = new ArrayList<>();
-        g.generate("this is simple text", 3, (String s) ->
-                result.add(s));
+        g.generate("this is simple text", s -> result.add(s));
+
         assertArrayEquals(new String[]{"thi", "his", "is", "sim", "imp", "mpl", "ple", "tex", "ext"}, result.toArray(new String[]{}));
     }
+
+    @Test
+    public void generate_too_small_grams() {
+        NGramGenerator g = new NGramGenerator(1);
+        assertThrows(IllegalArgumentException.class, () -> g.generate("this is simple text", s -> {
+        }));
+    }
+
 }
