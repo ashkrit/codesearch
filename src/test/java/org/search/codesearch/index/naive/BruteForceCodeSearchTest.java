ddca642920ee08f9bc6701c2b7f1f8de5ca26b8f
Ashkrit <ashkrit@gmail.com>
2020-07-26 11:40:50 +0800

    Refactor - Prepare test for contract based test

--- src/test/java/org/search/codesearch/index/naive/BruteForceCodeSearchTest.java
+++ src/test/java/org/search/codesearch/index/naive/BruteForceCodeSearchTest.java
@@ -15,37 +15,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BruteForceCodeSearchTest {
 
-    private List<String> rootPaths;
-
+    private Search codeSearch;
 
     @BeforeEach
     public void pathToSearch() throws URISyntaxException {
         Path p = Paths.get(this.getClass().getResource("/").toURI());
-        this.rootPaths = Arrays.asList(p.toFile().getAbsolutePath());
+        List<String> rootPaths = Arrays.asList(p.toFile().getAbsolutePath());
+        this.codeSearch = new BruteForceCodeSearch(rootPaths);
     }
 
     @Test
     public void no_result_match_for_random_string() {
-        Search s = new BruteForceCodeSearch(rootPaths);
+
         List<Path> result = new ArrayList<>();
-        s.match("gsak jksd ", x -> result.add(x), 10);
+        codeSearch.match("gsak jksd ", x -> result.add(x), 10);
         assertEquals(0, result.size());
     }
 
 
     @Test
     public void result_based_on_file_name() {
-        Search s = new BruteForceCodeSearch(rootPaths);
+
         List<Path> result = new ArrayList<>();
-        s.match("tinylog", x -> result.add(x), 10);
+        codeSearch.match("tinylog", x -> result.add(x), 10);
         assertEquals("tinylog.properties", result.get(0).toFile().getName());
     }
 
     @Test
     public void result_based_on_file_content() {
-        Search s = new BruteForceCodeSearch(rootPaths);
         List<Path> result = new ArrayList<>();
-        s.match("stacktrace", x -> result.add(x), 10);
+        codeSearch.match("stacktrace", x -> result.add(x), 10);
         assertEquals("tinylog.properties", result.get(0).toFile().getName());
     }
 
