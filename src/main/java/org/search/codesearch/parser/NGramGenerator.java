0e72a09b25b758d0f35a7f4a4c8ffa5e15baed9b
Ashkrit <ashkrit@gmail.com>
2020-07-26 12:50:58 +0800

    Generate 3 gram values

--- src/main/java/org/search/codesearch/parser/NGramGenerator.java
+++ src/main/java/org/search/codesearch/parser/NGramGenerator.java
@@ -5,10 +5,24 @@ import java.util.function.Consumer;
 public class NGramGenerator {
 
     public void generate(String text, int noOfGrams, Consumer<String> consumer) {
-        if (noOfGrams == 1) {
+        if (noOfGrams == 0) {
             for (String v : text.split(" ")) {
                 consumer.accept(v);
             }
         }
+
+        if (noOfGrams == 3) {
+            String[] words = text.split(" ");
+            for (String word : words) {
+                if (word.length() >= noOfGrams) {
+                    for (int index = 0; index + 3 <= word.length(); index++) {
+                        consumer.accept(word.substring(index, index + 3));
+                    }
+                } else {
+                    consumer.accept(word);
+                }
+            }
+        }
+
     }
 }
--- src/test/java/org/search/codesearch/parser/NGramParserTest.java
+++ src/test/java/org/search/codesearch/parser/NGramParserTest.java
@@ -12,11 +12,20 @@ public class NGramParserTest {
 
     @Test
     public void generate_no_grams() {
-
         NGramGenerator g = new NGramGenerator();
         List<String> result = new ArrayList<>();
-        g.generate("this is simple text", 1, (String s) ->
+        g.generate("this is simple text", 0, (String s) ->
                 result.add(s));
         assertArrayEquals(new String[]{"this", "is", "simple", "text"}, result.toArray(new String[]{}));
+    }
+
+
+    @Test
+    public void generate_3_grams() {
+        NGramGenerator g = new NGramGenerator();
+        List<String> result = new ArrayList<>();
+        g.generate("this is simple text", 3, (String s) ->
+                result.add(s));
+        assertArrayEquals(new String[]{"thi", "his", "is", "sim", "imp", "mpl", "ple", "tex", "ext"}, result.toArray(new String[]{}));
     }
 }
