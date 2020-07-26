6194fc0e0056834accacf39c86b321a189b38c98
Ashkrit <ashkrit@gmail.com>
2020-07-26 12:13:51 +0800

    Fix shell client args. Now algo can be passed from outside

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# codesearch
+# codesearch 
+Library for building code search
+
+# Features
+
+- Index source folders
+- Allow searching based on file names.
+- Allow searching based on file content.
+
+
+# How to run
+This project ships shell based client to explore code. It can launched using below command
+
+java org.search.codesearch.App -source code1path;code2path
+
+eg 
+java org.search.codesearch.App -source ..\github\jdk;..\github\h2database
--- src/main/java/org/search/codesearch/App.java
+++ src/main/java/org/search/codesearch/App.java
@@ -8,43 +8,48 @@ import org.search.codesearch.index.naive.BruteForceCodeSearch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.Function;
 
 
 public class App {
 
     private static final Logger logger = LoggerFactory.getLogger(App.class);
 
+    private static Map<String, Function<List<String>, Search>> searchAlgo = new HashMap<String, Function<List<String>, Search>>() {{
+        put("naive", x -> new BruteForceCodeSearch(x));
+        put("cache", x -> new CacheFileTreeCodeSearch(x, InMemoryFileContentMatcher.create()));
+    }};
+
     public static void main(String[] args) {
         Map<String, String> params = ArgsParser.cmdParams(args);
 
+        logger.info("Search params {}", params);
         String rootPath = params.get("source");
-        String term = params.get("term");
-        logger.info("Searching {} for term {}", rootPath, term);
-
         List<String> locations = Arrays.asList(rootPath.split(";"));
 
-        List<Search> searchAlgo = Arrays.asList(
-                new CacheFileTreeCodeSearch(locations, InMemoryFileContentMatcher.create()),
-                new BruteForceCodeSearch(locations)
-        );
+        String key = algoToUse(params);
+        Function<List<String>, Search> searchBuilder = searchAlgo.get(key);
+
+        Objects.requireNonNull(searchBuilder, String.format("Unable to find %s, supported algo are %s", key, searchAlgo.keySet()));
+
+        Search search = searchBuilder.apply(locations);
 
         logger.info("Start search now ....");
-
 
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             String p = scanner.nextLine();
             if (p.trim().isEmpty()) continue;
+            search.match(p.toLowerCase(), file -> logger.info("Found {}", file), 1000);
 
-            for (Search search : searchAlgo) {
-                search.match(p.toLowerCase(), file -> logger.info("Found {}", file), 1000);
-            }
         }
 
+    }
+
+    private static String algoToUse(Map<String, String> params) {
+        String algo = params.get("algo");
+        return (algo == null) ? "cache" : algo;
     }
 
 }
