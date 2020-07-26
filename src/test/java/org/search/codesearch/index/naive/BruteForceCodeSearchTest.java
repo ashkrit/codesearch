ddb551b9048e84ab99fafd54253b333b7fb8395c
Ashkrit <ashkrit@gmail.com>
2020-07-26 11:31:21 +0800

    Empty search result test added

---
+++ src/test/java/org/search/codesearch/index/naive/BruteForceCodeSearchTest.java
@@ -0,0 +1,35 @@
+package org.search.codesearch.index.naive;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.search.codesearch.index.Search;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BruteForceCodeSearchTest {
+
+    private List<String> rootPaths;
+
+
+    @BeforeEach
+    public void pathToSearch() throws URISyntaxException {
+        Path p = Paths.get(this.getClass().getResource("/").toURI());
+        this.rootPaths = Arrays.asList(p.toFile().getAbsolutePath());
+    }
+
+    @Test
+    public void no_result_match_for_random_string() {
+
+        Search s = new BruteForceCodeSearch(rootPaths);
+        List<Path> result = new ArrayList<>();
+        s.match("gsak jksd ", x -> result.add(x), 10);
+        assertEquals(0, result.size());
+    }
+}
